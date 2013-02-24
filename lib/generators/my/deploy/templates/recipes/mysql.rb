5e0127bd333779ee0658320e125a3db5ff182b08
Arley Lobato <arleylobato@gmail.com>
2013-02-24 00:56:15 -0300

    mysql_db

--- lib/generators/my/deploy/templates/recipes/mysql.rb
+++ lib/generators/my/deploy/templates/recipes/mysql.rb
@@ -2,7 +2,7 @@ set_default(:mysql_password) { Capistrano::CLI.password_prompt "MySQL Password: 
 
 namespace :mysql do
 
-  desc "Create mysql db - cap deploy:mysql:create_db -s pass=secret"
+  desc "Create mysql db - cap deploy:mysql:create_db"
   task :create_db, roles: :db do
     run "mysqladmin -u root -p#{mysql_password} create #{application}_production"
   end
