dff3bd74410d641227caa29f943ec26edb39b301
Arley Lobato <arleylobato@gmail.com>
2013-03-07 17:21:43 -0300

    Update

--- lib/generators/my/deploy/templates/deploy.rb
+++ lib/generators/my/deploy/templates/deploy.rb
@@ -1,9 +1,6 @@
 require "bundler/capistrano"
 
-load "config/recipes/base"
-load "config/recipes/rbenv"
 load "config/recipes/nginx"
-load "config/recipes/nodejs"
 load "config/recipes/check"
 load "config/recipes/unicorn"
 load "config/recipes/mysql"
@@ -33,33 +30,18 @@ set :default_environment, {
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
-  desc "Load schema"
-  task :load_schema, roles: :app do
-    rails_env = fetch(:rails_env, "production")
-    run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} db:schema:load"
-  end
-
-  desc "Cold deploy"
-  task :cold do # Overriding the default deploy:cold (http://stackoverflow.com/questions/1329778/dbschemaload-vs-dbmigrate-with-capistrano)
-    update
-    # load_schema
-    migrate
-    start_unicorn
-    autostart_unicorn
-    restart_nginx
-  end
 
   task :setup_config, roles: :app do
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-    # put File.read("config/config.example.yml"), "#{shared_path}/config/config.yml"
+    put File.read("config/config.example.yml"), "#{shared_path}/config/config.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    # run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
+    run "ln -nfs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 end
